11e8c858b988279fd4cdb00ba0d21022738894f0
Aakashkalla <aakashpradeepkalla@gmail.com>
2025-06-20 03:03:47 +0530

    Solved an Assignment Question

---
+++ Assignment Questions/Question9.java
@@ -0,0 +1,19 @@
+// Factorial Number
+import java.util.*;
+public class Question9 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter A Number:");
+        int n = in.nextInt();
+        facf(n);
+        
+    }
+    static void facf(int n){
+        int result = 1;
+        for(int i = 1;i<=n;i++){
+            result = result*i;
+           System.out.println(result);
+        }
+        
+    }
+}
