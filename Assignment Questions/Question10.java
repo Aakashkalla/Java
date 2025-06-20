2d2b06c32a4962b3c2809d1bace9ecc63f3645b6
Aakashkalla <aakashpradeepkalla@gmail.com>
2025-06-20 23:06:49 +0530

    Solved some assignment questions and a Leetcode Easy

---
+++ Assignment Questions/Question10.java
@@ -0,0 +1,26 @@
+//Area of triangle, rectangle and square:
+import java.util.*;
+public class Question10 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Welcome this program takes 2 input and prints triangle, rectangle and square area");
+        System.out.print("Enter 2 integers:");
+        double a = in.nextDouble();
+        double b = in.nextDouble();
+        areat(a, b);
+        areas(a);
+        arear(a,b);
+    }
+    static void areat(double a, double b){
+        double area = 0.5*a*b;
+        System.out.println("Area of triangle is: " + area );
+    }
+    static void areas(double a){
+        double areas = a*a;
+        System.out.println("Area of square is: " + areas);
+    }
+    static void arear(double a, double b){
+        double arear = a*b;
+        System.out.println("Area of rectangle is: " + arear);
+    }
+}
---
+++ Assignment Questions/Question11.java
@@ -0,0 +1,21 @@
+//Palindrome
+import java.util.*;
+public class Question11 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter Your number to check palindrome or not:");
+        int n = in.nextInt(); 
+        System.out.println(palindrome(n)); 
+    }
+    static boolean palindrome(int n){
+        int copy = n;
+        int s = 0;
+        while(n>0){
+            int r = n%10;
+            s = s*10+r;
+            n = n/10;
+
+        }
+        return s==copy;
+    }
+}
