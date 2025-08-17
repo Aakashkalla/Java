af7652a819dd706616d2a76aac551baab5a4031c
Aakashkalla <aakashpradeepkalla@gmail.com>
2025-08-17 23:18:03 +0530

    Solved 1 basic recursion Problem

---
+++ Day 7/CountZeroRecursion.java
@@ -0,0 +1,14 @@
+public class CountZeroRecursion {
+    public static void main(String[] args){
+        System.out.println(countZero(1002, 0));
+    }
+    static int countZero(int n, int count){
+        if(n==0){
+            return count;
+        }
+        if(n%10==0){
+            return countZero(n/10, count+1);
+        }
+        return countZero(n/10, count);
+    }
+}
